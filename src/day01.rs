c7ced066ab9202b2897ddf5cce8b29f7ce17ecfe
Nicolas Hery <hery.nicolas@gmail.com>
2017-12-05 14:47:02 -0500

    Solve Day 1 part one

--- src/day01.rs
+++ src/day01.rs
@@ -1,14 +1,26 @@
-pub fn capcha(input: Vec<u32>) -> u32 {
-    let len = input.len();
+pub fn captcha(input: &str) -> u32 {
+  sum_captcha(parse_input(input))
+}
+
+fn parse_input(input: &str) -> Vec<u32> {
+  input.chars()
+    .map(|c| {
+      c.to_digit(10).expect(&format!("{} is not a digit (0-9)", c))
+    })
+    .collect()
+}
+
+fn sum_captcha(digits: Vec<u32>) -> u32 {
+    let len = digits.len();
     if len == 0 {
         return 0;
     }
 
-    let first = input[0];
-    input.iter()
+    let first = digits[0];
+    digits.iter()
         .enumerate()
         .filter(|&(i, n)| {
-            let next = if i == len - 1 { first } else { input[i+1] };
+            let next = if i == len - 1 { first } else { digits[i+1] };
             *n == next
         })
         .map(|(_, n)| n)
--- tests/day01.rs
+++ tests/day01.rs
@@ -1,23 +1,23 @@
 extern crate adventofcode;
 
-use adventofcode::day01::capcha;
+use adventofcode::day01::captcha;
 
 #[test]
 fn solve_1122() {
-    assert_eq!(3, capcha(vec![1, 1, 2, 2]));
+    assert_eq!(3, captcha("1122"));
 }
 
 #[test]
 fn solve_1111() {
-    assert_eq!(4, capcha(vec![1, 1, 1, 1]));
+    assert_eq!(4, captcha("1111"));
 }
 
 #[test]
 fn solve_1234() {
-    assert_eq!(0, capcha(vec![1, 2, 3, 4]));
+    assert_eq!(0, captcha("1234"));
 }
 
 #[test]
 fn solve_91212129() {
-    assert_eq!(9, capcha(vec![9, 1, 2, 1, 2, 1, 2, 9]));
+    assert_eq!(9, captcha("91212129"));
 }
