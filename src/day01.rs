c175b3f581c8b9eaf1f1fba3e0cba9146f0b8e45
Nicolas Hery <hery.nicolas@gmail.com>
2017-12-07 14:04:51 -0500

    Move input to separate data file

--- src/day01.rs
+++ src/day01.rs
@@ -1,23 +1,26 @@
 pub enum Mode {
-  NextDigit,
-  HalfwayAround,
+    NextDigit,
+    HalfwayAround,
 }
 
 pub fn captcha(input: &str, mode: Mode) -> u32 {
-  let digits = parse_input(input);
-  let step = match mode {
-    Mode::NextDigit => 1,
-    Mode::HalfwayAround => half_len(&digits),
-  };
-  sum_captcha(digits, step)
+    let digits = parse_input(input);
+    let step = match mode {
+        Mode::NextDigit => 1,
+        Mode::HalfwayAround => half_len(&digits),
+    };
+    sum_captcha(digits, step)
 }
 
 fn parse_input(input: &str) -> Vec<u32> {
-  input.chars()
-    .map(|c| {
-      c.to_digit(10).expect(&format!("{} is not a digit (0-9)", c))
-    })
-    .collect()
+    input
+        .chars()
+        .map(|c| {
+            c.to_digit(10).expect(
+                &format!("{} is not a digit (0-9)", c),
+            )
+        })
+        .collect()
 }
 
 fn sum_captcha(digits: Vec<u32>, step: usize) -> u32 {
@@ -26,7 +29,8 @@ fn sum_captcha(digits: Vec<u32>, step: usize) -> u32 {
         return 0;
     }
 
-    digits.iter()
+    digits
+        .iter()
         .enumerate()
         .filter(|&(i, n)| {
             let next_i = (i + step) % len;
@@ -37,10 +41,13 @@ fn sum_captcha(digits: Vec<u32>, step: usize) -> u32 {
 }
 
 fn half_len(digits: &Vec<u32>) -> usize {
-  let len = digits.len();
-  if len % 2 != 0 {
-    panic!("Expected list of length {} to contain an even number of elements", len);
-  }
+    let len = digits.len();
+    if len % 2 != 0 {
+        panic!(
+            "Expected list of length {} to contain an even number of elements",
+            len
+        );
+    }
 
-  len / 2
+    len / 2
 }
